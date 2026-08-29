88d11c3f64ed0173245739c138df30682d115fd3
Nativon926 <39961225+Nativon926@users.noreply.github.com>
2026-08-28 21:27:12 -0400

    Add files via upload
    Renamed file for readability and understandability

---
+++ CountXX.java
@@ -0,0 +1,15 @@
+public class CountXX {
+    public static void main(String[] args) {
+        int count = 0;
+        int n = 2;
+        String str = "xxx";
+
+        for(int i = 0; i < n; i++) {
+            if(str.substring(i, i + 2).equals("xx")) {
+                count = count + 1;
+            }
+        }
+
+        System.out.println("the number of xx in " + str + " is " + count);
+    }
+}
